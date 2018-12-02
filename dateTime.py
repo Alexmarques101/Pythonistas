b10c8d0fba2be79272e13cf40f4d505e9e727123
Alexandre Vieira Marques dos Santos <alexandremarques101@hotmail.com>
2018-12-02 17:36:27 +0000

    Add files via upload

--- dateTime.py
+++ dateTime.py
@@ -3,28 +3,19 @@
 # 39071 Alexandre Santos
 # 53828 Vasco Oliveira
 
-from datetime import datetime
+import datetime as dt
 
-
-def date (date):
-    """
-    Write contract
-    """
-    date = datetime.strptime(date, '%Y-%m-%d') 
-    
-    
-    return date
-
-def hours (hours):
-    """
-    Write contract
-    """
-    hours = datetime.strptime(hours, '%H:%M').time()
-       
-    return hours
 
 def strToDate (date, hours):
     
-    date = datetime.strptime(date, '%Y-%m-%d') 
-    hours = datetime.strptime(hours, '%H:%M').time()
+    date = dt.datetime.strptime(date, '%Y-%m-%d') 
+    hours = dt.datetime.strptime(hours, '%H:%M').time()
     return date, hours
+
+#Function to add one hour to the finish time of expert task    
+def add_one_hour(hours):
+
+    t1 = dt.datetime.strptime(hours, '%H:%M')
+    t2 = dt.datetime.strptime('01:00', '%H:%M')
+    time_available = dt.datetime.strptime('00:00', '%H:%M')
+    return (t1 - time_available + t2).time().strftime('%H:%M')
--- scheduling.py
+++ scheduling.py
@@ -1,3 +1,4 @@
+
 # 2018-2019 Fundamentos de Programação (LTI)
 # Grupo 24 - Pythonistas
 # 39071 Alexandre Santos
@@ -7,7 +8,7 @@ from copy import deepcopy
 file1= '2019y02m15clients10h30.txt'
 file2 = '2019y02m15experts10h30.txt'
 from filesReading import readFile
-from dateTime import strToDate
+from dateTime import strToDate, add_one_hour
 
 def scheduling (file1, file2):
     """
@@ -21,17 +22,17 @@ def scheduling (file1, file2):
     for client in range(len(clients[1])): #itera sobre a lista dos clientes um a um
         for expert in range(len(experts[1])): #itera sobre a lista dos experts um a um
             
-            #O especialista a quem é atribuído o atendimento tem de ser um profissional no domínio do pedido,
-            #com um nível de reputação igual ou superior ao pretendido pelo cliente, a cobrar à hora, no máximo, aquilo que o cliente aceita pagar, 
-            #e a trabalhar na zona/cidade para que é feito o pedido.
+            #if the local is the same, if the service asked is addressed by expert,  quality of service (e.g. 4*),price asked by client higher than expert charges
             if (clients[1][client][1]== experts[1][expert][1]) and (clients[1][client][6] in experts [1][expert][2]) \
                 and (clients[1][client][5] <= experts [1][expert][3]) and (clients[1][client][5] <= experts [1][expert][3]) \
                 and (int(clients[1][client][4]) >= int(experts[1][expert][4])): #caso a cidade do cliente 1 coincida com a do expert 1 então devolve os dois, etc
                     
                     result= max(strToDate(clients[1][client][2],clients[1][client][3]),
-                              strToDate(experts[1][expert][5],experts[1][expert][6])),\
+                              max(strToDate(experts[1][expert][5],experts[1][expert][6]),strToDate(experts[0][0],experts[0][1]))),\
                             clients[1][client][0],experts[1][expert][0]
-                            
+                    
+                    if strToDate(experts[1][expert][5],experts[1][expert][6]) > strToDate(experts[0][0],experts[0][1]):
+                        result[0][1] == add_one_hour(strToDate(experts[1][expert][5],experts[1][expert][6])[1].strftime("%H:%M"))  #adding 1h to the expert availability                          
                     
                     final.append([result[0][0].strftime("%Y-%m-%d"), result[0][1].strftime("%H:%M"), result[1], result[2]])
                     
@@ -54,8 +55,3 @@ def scheduling (file1, file2):
 
     #Fiz uns testes mas parece que as datas não estão a sair bem...
     
-       
-           
-           
-           
-                
